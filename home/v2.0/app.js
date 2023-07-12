02b075261470e0d8c57ca8afa2024d3fa7bc77d5
Alf Equilfe <96450945+equneko@users.noreply.github.com>
2023-07-12 08:37:28 +0700

    Include data.js to app.js

--- home/v2.0/app.js
+++ home/v2.0/app.js
@@ -1,6 +1,32 @@
 const { app, use, ref, hook, init, render, xss } = Elode;
 
-const Logo = "res/images/logo.png";
+const Logo = "res/images/logo.png",
+      NavLinks = [
+    "[:docs_link ][aria-label Elode.js Documentation]Learn",
+    "[:docs_link ][aria-label Elode.js Documentation]Docs",
+    "[:docs_link ][aria-label Elode.js Documentation]Reference",
+    "[:link https://github.com/equneko/elode/tree/main/example/v2.0 ][aria-label Elode.js Example] Example",
+    "[:link https://chat.whatsapp.com/GnTCmu2sjGlGJs2cnt4GnH ][aria-label Elode.js Community] Community",
+    "[:github_link ][aria-label Elode.js Github] <span.medium.fa.fa-github>",
+    "[:link https://github.com/equneko/elode/stargazers ][aria-label Elode.js Stargazer Github] " +
+    "<span.fa.fa-star> <span[style font-size:9pt] {$stargazer}>",
+    "{html $darkSpan}",
+    "[:link https://github.com/equneko/elode/releases/tag/v2.0 ][aria-label Elode.js Release] " +
+    "<span.medium.fa.fa-download> <span[style font-size:9pt] v2.0>",
+    "{html $sideSpan}",
+],
+devs = [
+    "!res/images/mark.jpg",
+    "equneko",
+    "!res/images/sikochengoren.jpg",
+    "udinesia325",
+    "DimasAdiPutra",
+    "ezmemoar",
+    "galihapriansyah",
+    "rifkidocs",
+    "RaffiHDMedia",
+    "seonha14"
+].concat(Array(17).fill("none"));
 
 use('div', 'h1', 'p', 'input', 'span', 'ul', 'li', 'img', {
     link: 'a.btn[href #]'
