a73eda82b2bae6c9aaa7bf253676aa9f476d2643
Alf Equilfe <96450945+equneko@users.noreply.github.com>
2023-07-30 22:44:12 +0700

    Update app.js

--- home/v2.0/app.js
+++ home/v2.0/app.js
@@ -19,7 +19,7 @@ devs = [
     "!res/images/mark.jpg",
     "equneko",
     "!res/images/sikochengoren.jpg",
-    "!blob:https://web.whatsapp.com/86e68d9d-82c8-4209-a37f-7655a593ab57",
+    "!res/images/komeng.jpg",
     "udinesia325",
     "DimasAdiPutra",
     "ezmemoar",
